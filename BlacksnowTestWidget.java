9c990926a2e0ee6c8b29d1f37452caa9e94a4383
Martin Kitonga <119602263+blacksnowmartin@users.noreply.github.com>
2024-01-18 09:57:07 +0300

    Create BlacksnowTestWidget.java

---
+++ BlacksnowTestWidget.java
@@ -0,0 +1,10 @@
+public class BlacksnowTestWidget extends AppWidgetProvider {
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // Perform update logic here
+        // You can initiate the internet speed test when the widget is updated
+    }
+
+    // Add other methods and logic as needed
+  }
